8da595f8f139de605404273a25c375b4d3fdb556
Dylan Estrada <dylan@hotmail.com>
2021-10-24 15:15:48 -0500

    Carro de compras c sharp primera fase

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace CarritoCompras
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCarritoCompras = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnListProductos = new System.Windows.Forms.Button();
+            this.cboProductos = new System.Windows.Forms.ComboBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.btnDescuento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listCarritoCompras
+            // 
+            this.listCarritoCompras.FormattingEnabled = true;
+            this.listCarritoCompras.ItemHeight = 15;
+            this.listCarritoCompras.Location = new System.Drawing.Point(62, 44);
+            this.listCarritoCompras.Name = "listCarritoCompras";
+            this.listCarritoCompras.Size = new System.Drawing.Size(202, 274);
+            this.listCarritoCompras.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(301, 124);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnListProductos
+            // 
+            this.btnListProductos.Location = new System.Drawing.Point(402, 124);
+            this.btnListProductos.Name = "btnListProductos";
+            this.btnListProductos.Size = new System.Drawing.Size(150, 23);
+            this.btnListProductos.TabIndex = 2;
+            this.btnListProductos.Text = "Calcular precio";
+            this.btnListProductos.UseVisualStyleBackColor = true;
+            this.btnListProductos.Click += new System.EventHandler(this.btnListProductos_Click);
+            // 
+            // cboProductos
+            // 
+            this.cboProductos.FormattingEnabled = true;
+            this.cboProductos.Location = new System.Drawing.Point(301, 43);
+            this.cboProductos.Name = "cboProductos";
+            this.cboProductos.Size = new System.Drawing.Size(251, 23);
+            this.cboProductos.TabIndex = 3;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(301, 72);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(140, 23);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // btnDescuento
+            // 
+            this.btnDescuento.Location = new System.Drawing.Point(301, 176);
+            this.btnDescuento.Name = "btnDescuento";
+            this.btnDescuento.Size = new System.Drawing.Size(75, 23);
+            this.btnDescuento.TabIndex = 5;
+            this.btnDescuento.Text = "Codigo";
+            this.btnDescuento.UseVisualStyleBackColor = true;
+            this.btnDescuento.Click += new System.EventHandler(this.btnDescuento_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.btnDescuento);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.cboProductos);
+            this.Controls.Add(this.btnListProductos);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.listCarritoCompras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listCarritoCompras;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnListProductos;
+        private System.Windows.Forms.ComboBox cboProductos;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btnDescuento;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarritoCompras
+{
+    public partial class Form1 : Form
+    {
+        double precioFinal = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboProductos.Items.Add("Pantalon");
+            cboProductos.Items.Add("Camisa");
+            cboProductos.Items.Add("Zapatos");
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string productoSeleccionado = cboProductos.SelectedItem.ToString();
+            listCarritoCompras.Items.Add(productoSeleccionado);
+        }
+
+        private void btnListProductos_Click(object sender, EventArgs e)
+        {
+            
+            int totalProductosCargados = listCarritoCompras.Items.Count;
+
+            for(int i =0; i < totalProductosCargados; i++)
+            {
+                string productoElegido = listCarritoCompras.Items[i].ToString();
+                if (productoElegido == "Pantalon")
+                {
+                    precioFinal += 20;
+
+                }if (productoElegido == "Camisa")
+                {
+                    precioFinal += 35;
+                }
+                if(productoElegido== "Zapatos")
+                {
+                    precioFinal += 24;
+                }
+                
+            }
+            MessageBox.Show("el total de sus productos es " + precioFinal);
+        }
+
+        private void btnDescuento_Click(object sender, EventArgs e)
+        {
+            string codigo = txtCodigo.Text;
+            if(codigo == "NAVIDAD")
+            {
+                precioFinal = precioFinal * 0.90;
+                MessageBox.Show("el total de sus productos es " + precioFinal);
+            }
+            else
+            {
+                MessageBox.Show("codigo invalido");
+                MessageBox.Show("el total de sus productos es " + precioFinal);
+            }
+        }
+    }
+}
